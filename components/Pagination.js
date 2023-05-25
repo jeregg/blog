f1bb11c4ea507e5bf8f669f8644147f26e292635
JEREGG <2405348624@qq.com>
2023-05-26 01:20:51 +0800

    Update Pagination.js

--- components/Pagination.js
+++ components/Pagination.js
@@ -11,7 +11,7 @@ const Pagination = ({ page, showNext }) => {
   if (currentPage !== 1 && !showNext) additionalClassName = 'justify-start'
   return (
     <div
-      className={`flex font-medium text-black dark:text-gray-100 ${additionalClassName}`}
+      className={`flex text-sm text-grey-200 hover:text-grey-800 transition duration-100 ${additionalClassName}`}
     >
       {currentPage !== 1 && (
         <Link
