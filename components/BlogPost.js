27a2b8b8015f70b353b65468b08abdfa878af785
JEREGG <2405348624@qq.com>
2023-05-25 22:33:16 +0800

    Update BlogPost.js

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -9,10 +9,10 @@ const BlogPost = ({ post }) => {
     <Link href={`${BLOG.path}/${post.slug}`}>
       <article key={post.id} className="mb-6 md:mb-8">
         <header className="flex flex-col justify-between md:flex-row md:items-baseline">
-          <h2 className="text-lg md:text-xl font-semibold mb-2 cursor-pointer text-grey-800 dark:text-gray-100">
+          <h2 className="text-2xl md:text-2xl font-thin mb-2 cursor-pointer text-grey-800 dark:text-gray-100" hover:underline-offset-8>
             {post.title}
           </h2>
-          <time className="flex-shrink-0 text-base font-extralight text-gray-800 dark:text-gray-400">
+          <time className="flex-shrink-0 text-sm font-semibold text-gray-800 dark:text-gray-400">
             <FormattedDate date={post.date} />
           </time>
         </header>
