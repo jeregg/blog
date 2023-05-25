d3d7555fd4f55f777383f86cb870ffe776b8b2f4
JEREGG <2405348624@qq.com>
2023-05-25 22:49:52 +0800

    Update BlogPost.js

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -9,7 +9,7 @@ const BlogPost = ({ post }) => {
     <Link href={`${BLOG.path}/${post.slug}`}>
       <article key={post.id} className="mb-6 md:mb-8">
         <header className="flex flex-col justify-between md:flex-row md:items-baseline">
-          <h2 className="text-2xl md:text-2xl font-thin mb-2 cursor-pointer text-grey-800 dark:text-gray-100 underline hover:underline-offset-8">
+          <h2 className="text-2xl md:text-2xl font-thin mb-2 cursor-pointer text-grey-800 dark:text-gray-100 hover:underline-offset-8">
             {post.title}
           </h2>
           <time className="flex-shrink-0 text-sm text-gray-800 dark:text-gray-400">
@@ -17,7 +17,7 @@ const BlogPost = ({ post }) => {
           </time>
         </header>
         <main>
-          <p className="hidden md:block text-sm leading-6 text-gray-600 dark:text-gray-300">
+          <p className="hidden md:block text-sm leading-6 text-gray-800 dark:text-gray-300">
             {post.summary}
           </p>
         </main>
